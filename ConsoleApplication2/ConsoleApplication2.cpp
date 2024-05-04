ef5174a15aa7849980d23abcbb3e25d41decc456
Матвей <@DESKTOP-1MDIGEF>
2024-05-04 16:08:53 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+    int N;
+    int sum = 0;
+
+    std::cout << "Введите число N: ";
+    std::cin >> N;
+
+    for (int i = 1; i <= N; ++i) {
+        sum += i;
+    }
+
+    std::cout << "Сумма чисел от 1 до " << N << " равна " << sum << std::endl;
+
+    return 0;
